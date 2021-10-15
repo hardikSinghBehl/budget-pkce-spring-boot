45ef6bd81e5ec742f0854992f1230a4b1bfc0f60
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-15 16:02:11 +0530

    initial project commit

---
+++ src/main/java/com/behl/ehrmantraut/BudgetPkceApplication.java
@@ -0,0 +1,13 @@
+package com.behl.ehrmantraut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetPkceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetPkceApplication.class, args);
+	}
+
+}
