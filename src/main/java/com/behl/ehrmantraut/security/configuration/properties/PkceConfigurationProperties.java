c8e225b1358728bb650aff9c20dcc6dd6b04975b
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-15 17:32:49 +0530

    added security properties

---
+++ src/main/java/com/behl/ehrmantraut/security/configuration/properties/PkceConfigurationProperties.java
@@ -0,0 +1,24 @@
+package com.behl.ehrmantraut.security.configuration.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@ConfigurationProperties(prefix = "com.behl.ehrmantraut")
+@Data
+public class PkceConfigurationProperties {
+
+    private Security security = new Security();
+
+    @Data
+    public class Security {
+
+        private String clientId;
+        private String redirectUri;
+        private String responseType;
+        private String codeChallengeMethod;
+        private String grantType;
+
+    }
+
+}
